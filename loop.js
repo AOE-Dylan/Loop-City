973a85dca31a35277411cc1082edabda399a5f8d
AOE-Dylan <dylantt2000@gmail.com>
2016-11-30 09:04:14 -1000

    I haz done workd dis

--- loop.js
+++ loop.js
@@ -12,21 +12,26 @@ var adj = ["cool", "awesome", "purple", "green", "long nek"]
 
 
 for (var i = 0; i < partyAnimals.length; i++) {
-  for (var a = 0; a < adj.length; a++){
-    console.log(adj[a] + " " + partyAnimals[i] + " is in da house!")
+  for (var a = 0; a < 1; a++){
+    console.log(adj[i] + " " + partyAnimals[i] + " is in da house!")
   }
 }
 
 
-var weaponChest = ["Mace", "Sorceror's Stone", "Excalibur"]
+var weaponChest = ["Mace", "Sorceror's Stone", "Excalibur"];
 var hero = {
-  wizard : "Merlin"
-  weapon : weaponChest[1]
+  wizard : "Merlin",
+  weapon : weaponChest[0],
   power : 30
-}
+};
 
 function Battle(weapon) {
-  if (weaponChest = "Sorceror's Stone") {
-    console.log(hero.wizard + " shall slay")
-  } else {}
+  if (weapon == weaponChest[1]) {
+    console.log(hero.wizard + " shall slay the dragon with the " + weapon);
+  } else {
+    weapon = weaponChest[1];
+    console.log(hero.wizard + " is ready for war with the " + weapon);
+  }
 }
+
+Battle(hero.weapon);
